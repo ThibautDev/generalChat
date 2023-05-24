b81e67453907e7ecbae637a4d8bb810d2801078e
ThibautDev <thibaut.props@icloud.com>
2023-05-24 14:58:00 +0100

    add send message

--- html/assets/functions/chat/send.php
+++ html/assets/functions/chat/send.php
@@ -1,3 +1,16 @@
 <?php
-    echo date('H:i');
+    include '../conn.php';
+
+    session_start();
+
+    $user_id = $_SESSION["id"];
+    $message = $_POST["message"];
+    $language = $_POST["language"];
+
+    $currentTime = date('H:i'); 
+    $sendSql = "INSERT INTO $language (userId, message, time) VALUES ('$user_id', '$message', '$currentTime')";
+
+    conn("main")->query($sendSql);
+    conn("main")->close();
 ?>
+
